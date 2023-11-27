2a6d1e7a1cfb03a67a67c9cdf970665262062525
globeele <nsetko@yahoo.com>
2023-11-27 20:55:53 +0300

    n2 complete

---
+++ n2.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+class Answer {
+    public int[] subArrays(int[] a, int[] b){
+      // Проверяем, равны ли длины массивов a и b
+        if (a.length != b.length) {
+            // Если не равны, создаем и возвращаем
+            // новый массив c длины 1, содержащий только значение 0
+            return new int[]{0};
+        }
+        // Создаем новый массив c длины, равной длине массива a или b
+        int[] c = new int[a.length];
+        // Проходим по каждой ячейке массивов a и b,
+        // вычисляя разность их элементов и сохраняя эту разность в массиве c
+        for (int i = 0; i < a.length; i++) {
+            c[i] = a[i] - b[i];
+        }
+        // Возвращаем массив c
+        return c;     
+    }
+}
+
+// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+public class Printer{ 
+    public static void main(String[] args) { 
+      int[] a = {};
+      int[] b = {};
+      
+      if (args.length == 0) {
+        // При отправке кода на Выполнение, вы можете варьировать эти параметры
+        a = new int[]{4, 5, 6};
+        b = new int[]{1, 2, 3};
+      }
+      else{
+        a = Arrays.stream(args[0].split(", ")).mapToInt(Integer::parseInt).toArray();
+        b = Arrays.stream(args[1].split(", ")).mapToInt(Integer::parseInt).toArray();
+      }     
+      
+      Answer ans = new Answer(); 
+      String itresume_res = Arrays.toString(ans.subArrays(a, b));     
+      System.out.println(itresume_res);
+    }
+}
